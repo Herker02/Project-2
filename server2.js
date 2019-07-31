0f64eeed99b53db62dce8e92ea3d800916461e87
Htran_14 <hao.allen.tran@gmail.com>
2019-07-30 19:08:56 -0500

    new upload

--- server2.js
+++ server2.js
@@ -1,37 +1,39 @@
-var express = require('express');
-var fileUpload = require('express-fileupload');
+var express = require("express");
+var fileUpload = require("express-fileupload");
 var app = express();
+var path = require("path");
 
 var PORT = 3000;
-app.use('/form', express.static(__dirname + '/index.html'));
+app.use("/form", express.static(__dirname + "/index.html"));
 
 // default options
 app.use(fileUpload());
+//app.use('/upload', express.static(__dirname + '/upload.html'));
 
-
-app.post('/upload', function (req, res) {
+app.post("/upload", function(req, res) {
+  res.sendFile(path.join(__dirname, "/upload.html"));
   var sampleFile;
   var uploadPath;
 
-  if (Object.keys(req.files).length == 0) {
-    res.status(400).send('No files were uploaded.');
+  if (Object.keys(req.files).length === 0) {
+    res.status(400).send("No files were uploaded.");
     return;
   }
 
-  console.log('req.files >>>', req.files);
+  console.log("req.files >>>", req.files);
   sampleFile = req.files.sampleFile;
 
-  uploadPath = __dirname + '/public/uploads/' + sampleFile.name;
+  uploadPath = __dirname + "/public/uploads/" + sampleFile.name;
 
-  sampleFile.mv(uploadPath, function (err) {
+  sampleFile.mv(uploadPath, function(err) {
     if (err) {
       return res.status(500).send(err);
     }
-
-    res.send('File uploaded to ' + uploadPath);
+    console.log("return to main page");
+    //res.send("File uploaded to " + uploadPath);
   });
 });
 
-app.listen(PORT, function () {
-  console.log('Express server listening on port ', PORT);
+app.listen(PORT, function() {
+  console.log("Express server listening on port ", PORT);
 });
